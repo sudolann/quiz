5f855cdfc57bcfd36f472d79b21ca7ac2cb5a133
Admin <admin@MacBook-Pro-Admin.local>
2020-09-27 13:44:13 +0200

    question card

---
+++ src/components/questionCard/QuestionCard.tsx
@@ -0,0 +1,50 @@
+import React, { FunctionComponent, ReactElement, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'antd';
+import { getQuizData } from '../../state/quiz/selectors';
+import { QuizState } from '../../state/quiz/reducer';
+import { setScore } from '../../state/points/actions';
+import { getPoints } from '../../state/points/selectors';
+import { Results } from '../results/Results';
+import './QuestionCard.scss';
+
+export const QuestionCard: FunctionComponent = (): ReactElement => {
+  const dispatch = useDispatch();
+  const quiz: QuizState = useSelector(getQuizData);
+  const points: Array<0 | 1> = useSelector(getPoints);
+  const list = quiz.list;
+
+
+  const handleClick = (answer: boolean) => {
+    const correctAnswer = list[points.length].correct_answer === 'True' ? true : false;
+    const point = correctAnswer === answer ? 1 : 0;
+    dispatch(setScore(point));
+  };
+
+  if (list.length === points.length) {
+    return <Results />;
+  }
+  return (
+    <div className='question'>
+      <div className='question__header'>
+        <h1>{list[points.length]?.category}</h1>
+      </div>
+      <div className='question__card'>
+        <p>{list[points.length]?.question}</p>
+        <div className='question__card__buttons'>
+          <Button type='primary' ghost id='quiz-btn' onClick={(): void => handleClick(true)}>
+            true
+          </Button>
+          <Button type='primary' ghost id='quiz-btn' onClick={(): void => handleClick(false)}>
+            false
+          </Button>
+        </div>
+      </div>
+      <div className='question__footer'>
+        <p>
+          {points.length + 1} of {list.length}
+        </p>
+      </div>
+    </div>
+  );
+};
